7b950f886d04315c2586992df883aae50e58140a
CeNan11 <johnrenanlabay75@gmail.com>
2025-07-23 13:11:48 +0800

    added background gradient

---
+++ src/Component.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react"; 
+
+function Component(){
+        return(
+            //Main Container here
+            <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 flex items-center justify-center">
+                <div className="text-white text-center">
+                    <h1>To do List Checker</h1>
+                    <p>Organize task in a vibrant way</p>
+                </div>
+
+            </div>
+        );
+
+}
+
+export default Component;
