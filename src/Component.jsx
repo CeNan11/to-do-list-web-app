25d3b4bf960da57f5d2514d0beacb26fdab10ec4
CeNan11 <johnrenanlabay75@gmail.com>
2025-07-23 15:29:23 +0800

    added add and handle event functions

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,17 +1,44 @@
 import { useState, useEffect } from "react"; 
 
 function Component(){
+    const [task,setTask] = useState("");
+    const [newTask, setNewTask] = useState([]);
+
+    function handleEventChange(e){
+        setTask(e.target.value);
+    }
+    function addTask(){
+        if(task.trim() === "") return;
+        setNewTask([...newTask, task])
+        setTask("");
+    }
         return(
             //Main Container here
-            <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 flex items-center justify-center">
+            <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 flex flex-col items-center justify-center">
                 <div className="text-white text-center">
-                    <h1>To do List Checker</h1>
-                    <p>Organize task in a vibrant way</p>
+                    <h1 className="font-dancing text-6xl text-white">To do List Checker</h1>
+                    <p className="text-gray-100 font-sans">Organize task in a vibrant way</p>
                 </div>
-
+                {/*Container for the task*/}
+                <div className="text-white font-inter">
+                    <input
+                        type="text"
+                        value={task}
+                        onChange={handleEventChange}
+                        placeholder="Enter task"
+                        className="text-black"
+                    />
+                    <button onClick={addTask}>Add</button>
+                    <div>
+                        <ul>
+                           {newTask.map((task, index) => (
+                            <li key={index}>{task}</li>
+                        ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
-
 }
 
 export default Component;
