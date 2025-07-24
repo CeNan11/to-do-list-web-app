2d3a8bd4dd7b1a7b0b4900dfd9ac393a06e2ecf9
CeNan11 <johnrenanlabay75@gmail.com>
2025-07-24 15:33:27 +0800

    added some animations for interactivity

--- src/Component.jsx
+++ src/Component.jsx
@@ -34,30 +34,32 @@ function Component(){
         return(
             //Main Container here
             <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 flex flex-col items-center justify-center">
-                <div className="text-white text-center mb-10">
+                <div className="text-white text-center mb-10 hover:animate-wiggle">
                     <h1 className="font-dancing text-8xl text-white">To do List Checker</h1>
                     <p className="text-gray-100 font-sans">Organize task in a vibrant way</p>
                 </div>
                 {/*Container for the task*/}
                 <div className="text-white mt-10 font-inter text-center">
-                    <input
-                        type="text"
-                        value={task}
-                        onChange={handleEventChange}
-                        placeholder="Enter task"
-                        className="text-black border-b border-white px-4 py-2 focus:outline-none rounded-lg"
-                    />
-                    <button onClick={addTask} className="m-3">Add</button>
+                    <div className="transition-transform duration-200 hover:scale-95">
+                        <input
+                            type="text"
+                            value={task}
+                            onChange={handleEventChange}
+                            placeholder="Enter task"
+                            className="text-black border-b border-white px-4 py-2 focus:outline-none rounded-lg "
+                        />
+                        <button onClick={addTask} className="m-3">Add</button>
+                    </div>
                     {/*Container for the  tasks*/}
                     <div className="mt-4 space-y-2 w-full m-w-md flex items-center">
                         <ul>
                            {newTask.map((task, index) => (
                             <li key={index}
-                                className="flex items-start justify-between text-white w-full bg-white/10 rounded-lg mt-3 mb-2 py-4 px-2"
+                                className="transition-transform duration-200 hover:scale-125 flex items-center justify-between text-white w-90 bg-white/10 rounded-lg mt-3 mb-2 py-4 px-2"
                             >
                                 <span className="font-inter flex-1 pr-4 break-words whitespace-normal line-clamp-2">{task}</span>
                                 <div className="flex gap-3 ml-10 shrink-0 self-start">
-                                    <button className="transition-transform duration-200 hover:scale-125" onClick={() => moveTaskUp(index)}><FaArrowUp/></button>
+                                    <button className="transition-transform duration-500 hover:scale-125" onClick={() => moveTaskUp(index)}><FaArrowUp/></button>
                                     <button className="transition-transform duration-200 hover:scale-125" onClick={() => moveTaskDown(index)}><FaArrowDown/></button>
                                     <button className="transition-transform duration-200 hover:scale-125" onClick={() => deleteTask(task, index)}><FaTrash/></button>
                                 </div>
